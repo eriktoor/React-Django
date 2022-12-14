769185c93054b15b95e3fb5a6dcc63511b48e9d9
Erik Toor <eriktoor@gmail.com>
2022-12-13 21:54:49 -0500

    new

--- buildfile.sh
+++ buildfile.sh
@@ -1,5 +1,5 @@
 #buildfile.sh
-echo "hello world"
+echo "hello world erik"
 pip3 install -r music_controller/requirements.txt
 python3.9 music_controller/manage.py makemigrations 
 python3.9 music_controller/manage.py migrate 
