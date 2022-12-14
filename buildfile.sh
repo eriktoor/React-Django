4b69a8887dab786e1a897584be94fbb2af646bcc
Erik Toor <eriktoor@gmail.com>
2022-12-13 21:31:52 -0500

    fixing buildfile

--- buildfile.sh
+++ buildfile.sh
@@ -1,4 +1,5 @@
 #buildfile.sh
+sudo apt-get install libsqlite3-dev
 pip3 install -r music_controller/requirements.txt
 python3 music_controller/manage.py makemigrations 
 python3 music_controller/manage.py migrate 
