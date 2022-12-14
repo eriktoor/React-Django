a93c2615666b83bc479712f34600b5e1abbc6b44
Erik Toor <eriktoor@gmail.com>
2022-12-13 21:22:08 -0500

    buildfile

--- buildfile.sh
+++ buildfile.sh
@@ -1,3 +1,5 @@
 #buildfile.sh
 pip3 install -r music_controller/requirements.txt
+python3 music_controller/manage.py makemigrations 
+python3 music_controller/manage.py migrate 
 python3 music_controller/manage.py runserver 
