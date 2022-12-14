39ddafd9f917493b6929871b60c51d3a46a54056
Erik Toor <eriktoor@gmail.com>
2022-12-13 21:47:41 -0500

    new

--- buildfile.sh
+++ buildfile.sh
@@ -1,8 +1,6 @@
 #buildfile.sh
 echo "hello world"
-sudo apt-get install libsqlite3-dev libsqlite3
-echo "after requirements"
 pip3 install -r music_controller/requirements.txt
-python3 music_controller/manage.py makemigrations 
-python3 music_controller/manage.py migrate 
-python3 music_controller/manage.py runserver 
+python3.9 music_controller/manage.py makemigrations 
+python3.9 music_controller/manage.py migrate 
+python3.9 music_controller/manage.py runserver 
