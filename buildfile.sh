9f1621a9aa26a375c6d58dc3ba4cc7b805c709ea
Erik Toor <eriktoor@gmail.com>
2022-12-13 21:38:55 -0500

    new deploy

--- buildfile.sh
+++ buildfile.sh
@@ -1,5 +1,5 @@
 #buildfile.sh
-sudo apt-get install libsqlite3-dev
+sudo apt-get install libsqlite3-dev libsqlite3
 pip3 install -r music_controller/requirements.txt
 python3 music_controller/manage.py makemigrations 
 python3 music_controller/manage.py migrate 
